8836e50dab3fdb6f09a198773e2501815aaaf354
Xavier-Frédéric Moulet <xavier-frederic.moulet@orange.com>
2022-07-28 15:29:59 +0200

    Separate board to own file

---
+++ software/horloge/src/board.rs
@@ -0,0 +1,102 @@
+
+// See https://stackoverflow.com/questions/71653128/use-of-generics-for-embedded-hal-structs
+
+use horloge::data::*;
+
+use stm32f4xx_hal as hal;
+
+use hal::{pac, prelude::*};
+use panic_halt as _; // panic handler
+
+pub struct Board {
+    delay: hal::timer::SysDelay,
+    rtc: hal::rtc::Rtc,
+
+    line1: hal::gpio::Pin<'A',0,hal::gpio::Output>,
+    line2: hal::gpio::Pin<'A',1,hal::gpio::Output>,
+    line3: hal::gpio::Pin<'A',2,hal::gpio::Output>,
+    line4: hal::gpio::Pin<'A',3,hal::gpio::Output>,
+    line5: hal::gpio::Pin<'A',4,hal::gpio::Output>,
+    line6: hal::gpio::Pin<'B',7,hal::gpio::Output>,
+
+    column1: hal::gpio::Pin<'B',3,hal::gpio::Output>,
+    column2: hal::gpio::Pin<'A',11,hal::gpio::Output>,
+    column3: hal::gpio::Pin<'B',0,hal::gpio::Output>,
+    column4: hal::gpio::Pin<'A',7,hal::gpio::Output>,
+    column5: hal::gpio::Pin<'A',6,hal::gpio::Output>,
+    column6: hal::gpio::Pin<'B',5,hal::gpio::Output>,
+}
+
+impl Board {
+    pub fn new() -> Board {
+        let mut dp = pac::Peripherals::take().unwrap();
+        let cp = cortex_m::peripheral::Peripherals::take().unwrap();
+
+        // Set up the LEDs
+        let gpioa = dp.GPIOA.split();
+        let gpiob = dp.GPIOB.split();
+
+        // Set up the system clock. We want to run at 48MHz for this one.
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+
+        Board{
+            line1: gpioa.pa0.into_push_pull_output(),
+            line2: gpioa.pa1.into_push_pull_output(),
+            line3: gpioa.pa2.into_push_pull_output(),
+            line4: gpioa.pa3.into_push_pull_output(),
+            line5: gpioa.pa4.into_push_pull_output(),
+            line6: gpiob.pb7.into_push_pull_output(),
+
+            column1: gpiob.pb3.into_push_pull_output(),
+            column2: gpioa.pa11.into_push_pull_output(),
+            column3: gpiob.pb0.into_push_pull_output(),
+            column4: gpioa.pa7.into_push_pull_output(),
+            column5: gpioa.pa6.into_push_pull_output(),
+            column6: gpiob.pb5.into_push_pull_output(),
+
+            delay:cp.SYST.delay(&clocks),
+            rtc: hal::rtc::Rtc::<hal::rtc::Lse>::new(dp.RTC, &mut dp.PWR), // lots of ':'
+        }
+    }
+
+    // light a LED for a small delay
+    pub fn light_led(&mut self, led: Option<LED>) {
+        // light correct LED in matrix
+        if let Some(l) = led {
+            let (line, column) = LED_POSITIONS[l as usize];
+
+            if line==0 {self.line1.set_high()} else {self.line1.set_low()};
+            if line==1 {self.line2.set_high()} else {self.line2.set_low()};
+            if line==2 {self.line3.set_high()} else {self.line3.set_low()};
+            if line==3 {self.line4.set_high()} else {self.line4.set_low()};
+            if line==4 {self.line5.set_high()} else {self.line5.set_low()};
+            if line==5 {self.line6.set_high()} else {self.line6.set_low()};
+
+            if column==0 {self.column1.set_high()} else {self.column1.set_low()};
+            if column==1 {self.column2.set_high()} else {self.column2.set_low()};
+            if column==2 {self.column3.set_high()} else {self.column3.set_low()};
+            if column==3 {self.column4.set_high()} else {self.column4.set_low()};
+            if column==4 {self.column5.set_high()} else {self.column5.set_low()};
+            if column==5 {self.column6.set_high()} else {self.column6.set_low()};
+        }
+
+        self.delay.delay_us(10 as u32);
+    }
+
+    /// this function returns the current time in RTC as H:M5:M (NOT H:M:S !) where
+    ///     H is the hour 0-23
+    ///     M5 is the 5-minutes number 0-11
+    ///     M is the minutes 0-4
+    pub fn time(&self) -> (u8, u8, u8) {
+        // directly read register since we dont need date, sync
+        let tr = self.rtc.regs.tr.read();
+
+        let hour = tr.ht().bits() * 10 + tr.hu().bits();
+        let min5 = tr.mnt().bits() * 2 + (if tr.mnu().bits() >= 5 { 1 } else { 0 });
+        let units = tr.mnu().bits();
+        let minute = if units >= 5 { units - 5 } else { units }; // min % 5
+
+        (hour, min5, minute)
+    }
+}
--- software/horloge/src/main.rs
+++ software/horloge/src/main.rs
@@ -1,168 +1,21 @@
-mod data;
-use crate::data::*;
-
-const TICKS_SEC: u32 = 32768;
-const MAX_LEDS: usize = 32; // max LEDs "at a time" (ie muxed) - faster to use a power of two
+#![no_std]
+#![no_main]
 
 
-#[derive(Debug)]
-struct Horloge {
-    ticks: u32,  // 1/32KHz seconds. 0..TICKS_SEC * 60 
-    minutes: u8, // TICKS_SEC*60 ticks: 0..4
-    min5: u8,    // 5 minutes: 0..11
-    hours: u8,   // 0..23
-}
+mod board;
 
-impl Horloge {
-    const fn from_hm(hours: u8, min: u8) -> Horloge {
-        Horloge {
-            ticks: 0,
-            minutes: min % 5,
-            min5: min / 5,
-            hours,
+use horloge::MAX_LEDS;
+use crate::board::Board;
+use horloge::led_multiplex;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {    
+    let mut b  = Board::new();
+    loop {
+        let (hour, min5, minute) = b.time();
+        for mux_tick in 0..MAX_LEDS {
+            let led = led_multiplex(mux_tick, hour, min5, minute);
+            b.light_led(led);
         }
     }
-
-    // advance time ; hard RT @ TICKS_SEC Hz
-    fn tick(&mut self, button: bool) {
-        self.ticks += 1;
-        if self.ticks == TICKS_SEC * (if button { 1 } else { 60 }) {
-            self.ticks = 0;
-            self.minutes += 1;
-            if self.minutes == 5 {
-                self.min5 += 1;
-                self.minutes = 0;
-                if self.min5 == 12 {
-                    self.hours += 1; // change a la demie pour "moins le" ?
-                    self.min5 = 0;
-                }
-            }
-        }
-    }
-
-    // for a given tick, return the LED to illuminate - soft RT
-    fn led(&self) -> Option<LED> {
-        // moins le -> heure suivante, attention a minuit
-        let hours = if self.min5 > 6 {
-            if self.hours < 23 {
-                self.hours + 1
-            } else {
-                0
-            }
-        } else {
-            self.hours
-        };
-
-        let mux_tick = (self.ticks as usize) % MAX_LEDS;
-
-        // not a match since we dont have exclusive ranges
-        if mux_tick < NB_HOURS_LED {
-            HOURS_LED[hours as usize][mux_tick]
-        } else if mux_tick < NB_HOURS_LED+NB_MIN5_LED {
-            MIN5_LED[self.min5 as usize][mux_tick - NB_HOURS_LED]
-        } else if mux_tick < NB_HOURS_LED+NB_MIN5_LED+NB_MINUTES_LED {
-            MINUTES_LED[self.minutes as usize][mux_tick - NB_HOURS_LED - NB_MIN5_LED]
-        } else {
-            None
-        }
-    }
-
-    // debug code: show 2*MAX_LEDS ticks of leds
-    fn show_leds(&mut self) {
-        print!("{:?}:", self);
-        for _ in 0..MAX_LEDS*2 {
-            print!("{:?} ", self.led());
-            self.tick(false);
-        }
-        println!();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::*;
-
-    #[test]
-    fn create_tick_zero() {
-        let h = Horloge::from_hm(11,11);
-        assert_eq!(h.ticks,0);
-    }
-
-    #[test]
-    fn create_minute_ok() {
-        let h = Horloge::from_hm(12,34);
-        assert_eq!(h.minutes,4);
-        assert_eq!(h.min5,6);
-    }    
-
-    #[test]
-    fn create_hour() {
-        let h = Horloge::from_hm(12,34);
-        assert_eq!(h.hours,12);
-    }
-
-    #[test]
-    fn test_tick() {
-        let test_vec = [(2,0,0),(500,0,0),(32768,0,0),(32768*60-1,0,0),(32768*60,1,0),(32768*60*5,0,1)];
-        for (ti,min,min5) in test_vec {
-            let mut h = Horloge::from_hm(12,33);
-            for _ in 0..ti {
-                h.tick(false);
-            }
-            assert_eq!(h.minutes,3+min);
-            assert_eq!(h.min5,6+min5);
-        }
-    }
-
-    #[test]    
-    fn test_tick_fast() {
-        // when settings is set, time goes 60x faster
-        let test_vec = [(2,0,0),(500,0,0),(32768-1,0,0),(32768,1,0),(32768*5,0,1),(32768*60,0,0)];
-        for (ti,min,min5) in test_vec {
-            let mut h = Horloge::from_hm(0,0);
-            for _ in 0..ti {
-                h.tick(true);
-            }
-            println!("{:?}",ti);
-            assert_eq!(h.minutes,min);
-            assert_eq!(h.min5,min5);
-        }
-    }
-
-    #[test]
-    fn test_max_leds_should_be_enough() {
-        // should probably be a const assert
-        assert!(MAX_LEDS >= NB_HOURS_LED + NB_MIN5_LED + NB_MINUTES_LED);
-    }
-
-    #[test]
-    fn test_led_midnight() {
-        let mut h = Horloge::from_hm(0,0);
-        let leds = [Some(LED::Minuit),Some(LED::Minuit),Some(LED::Minuit),None,None,None];
-        for l in leds.iter() {
-            assert_eq!(h.led(), *l);
-            h.tick(false);
-        }
-    }
-
-    #[test]
-    fn test_led_12_57() {
-        let mut h = Horloge::from_hm(12,57);
-        let leds = [
-            Some(LED::Une),Some(LED::Heure),Some(LED::Heure),Some(LED::Heure),None,None,None,
-            Some(LED::Moins),Some(LED::Moins),Some(LED::Moins),Some(LED::CinqMin),Some(LED::CinqMin),None
-            ];
-        for l in leds.iter() {
-            assert_eq!(h.led(), *l);
-            h.tick(false);
-        }
-    }
-}
-
-fn main() {
-    Horloge::from_hm(0, 5).show_leds();
-    Horloge::from_hm(23, 37).show_leds();
-    Horloge::from_hm(11, 59).show_leds();
-    Horloge::from_hm(23, 33).show_leds();
-    Horloge::from_hm(12, 57).show_leds();
 }
