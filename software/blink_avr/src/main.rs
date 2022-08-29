372754a0843df0140f0dd759ef1a59a7fa27f2e7
Xavier-Frédéric Moulet <xavier-frederic.moulet@orange.com>
2022-08-29 22:46:01 +0200

    added avr blinker

---
+++ software/blink_avr/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+use arduino_hal::prelude::*;
+use arduino_hal::port::mode::Output;
+use arduino_hal::hal::port::PB5;
+
+fn stutter_blink(led: &mut arduino_hal::port::Pin<Output,PB5>, times: usize) {
+    (0..times).map(|i| i * 10).for_each(|i| {
+        led.toggle();
+        arduino_hal::delay_ms(i as u16);
+    });
+}
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let peripherals = arduino_hal::Peripherals::take().unwrap();
+
+    let pins = arduino_hal::pins!(peripherals);
+
+    let mut led = pins.d13.into_output();
+
+    loop {
+        stutter_blink(&mut led, 25);
+    }
+}
