5a9f3e3b2acba6cef8eb80de722ec257caf3ac2a
Xavier-Frédéric Moulet <xavier-frederic.moulet@orange.com>
2022-11-17 11:15:08 +0100

    tentative de faire fonctionner blink rust sur atmega8

--- software/blink_avr/src/main.rs
+++ software/blink_avr/src/main.rs
@@ -1,27 +1,28 @@
+// makes L6 LED blink
+
 #![no_std]
 #![no_main]
 
 use panic_halt as _;
 
-use arduino_hal::port::mode::Output;
-use arduino_hal::hal::port::PB5;
+use atmega_hal::delay::Delay;
+use embedded_hal::blocking::delay::DelayMs;
 
-fn stutter_blink(led: &mut arduino_hal::port::Pin<Output,PB5>, times: usize) {
-    (0..times).map(|i| i * 10).for_each(|i| {
-        led.toggle();
-        arduino_hal::delay_ms(i as u16);
-    });
-}
+type ClockSpeed = atmega_hal::clock::MHz8;
 
-#[arduino_hal::entry]
+#[no_mangle]
 fn main() -> ! {
-    let peripherals = arduino_hal::Peripherals::take().unwrap();
+    let peripherals = atmega_hal::Peripherals::take().unwrap();
+    let pins = atmega_hal::pins!(peripherals);
 
-    let pins = arduino_hal::pins!(peripherals);
+    let mut delay = Delay::<ClockSpeed>::new();
 
-    let mut led = pins.d13.into_output();
+    let mut led = pins.pd5.into_output();
 
     loop {
-        stutter_blink(&mut led, 25);        
+        led.set_high();
+        delay.delay_ms(300_u16);
+        led.set_low();
+        delay.delay_ms(300_u16);
     }
 }
