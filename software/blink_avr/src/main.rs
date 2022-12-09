127fc3a3d24a3b48d57e72db9f09e81fccd48edf
Xavier-Frédéric Moulet <xavier-frederic.moulet@orange.com>
2022-12-09 19:06:08 +0100

    blink avr now works (but using a low level library)

--- software/blink_avr/src/main.rs
+++ software/blink_avr/src/main.rs
@@ -1,28 +1,50 @@
-// makes L6 LED blink
+// makes L6 LED blink using very low-level code (no HAL so far)
 
 #![no_std]
 #![no_main]
 
+
 use panic_halt as _;
+use avr_device::atmega8::Peripherals;
 
-use atmega_hal::delay::Delay;
-use embedded_hal::blocking::delay::DelayMs;
+// absolutely inaccurate but eh
+#[inline(always)]
+fn delay(ms: u32) {
+    for _ in 0..ms * 1000 {
+        core::hint::black_box({}) // empty loop
+    }
+}
 
-type ClockSpeed = atmega_hal::clock::MHz8;
+struct BoardLED {
+    dp: Peripherals,
+}
 
-#[no_mangle]
+impl BoardLED {
+    fn new() -> BoardLED {
+        let b = BoardLED{ dp: Peripherals::take().unwrap() };
+        // directly configure bit 5 of PORTD as output
+        b.dp.PORTD.ddrd.write(|w| w.pd5().set_bit());
+        b
+    }
+    fn set(&self, b : bool) {
+        self.dp.PORTD.portd.write(
+            |w| if b { w.pd5().set_bit() } else { w.pd5().clear_bit() }
+        );
+    }
+}
+
+#[avr_device::entry]
 fn main() -> ! {
-    let peripherals = atmega_hal::Peripherals::take().unwrap();
-    let pins = atmega_hal::pins!(peripherals);
-
-    let mut delay = Delay::<ClockSpeed>::new();
-
-    let mut led = pins.pd5.into_output();
+    let led = BoardLED::new();
 
     loop {
-        led.set_high();
-        delay.delay_ms(300_u16);
-        led.set_low();
-        delay.delay_ms(300_u16);
+      delay(100);
+      led.set(false);
+      delay(100);
+      led.set(true);
+      delay(100);
+      led.set(false);
+      delay(500);
+      led.set(true);
     }
 }
