332e9e21c881fa133daeeb450a71a0129dec3df3
makapuf <makapuf2@gmail.com>
2023-01-21 18:05:50 +0100

    Make demo a function, run it once.

--- software/horloge/src/main.rs
+++ software/horloge/src/main.rs
@@ -3,35 +3,36 @@
 
 mod board;
 
-use crate::board::new_board;
+use crate::board::{new_board, BoardLEDs, BoardTimer};
 use horloge::led_multiplex;
 use horloge::MAX_LEDS;
 
+
+fn demo(board_leds: &BoardLEDs, board_timer: &BoardTimer) {
+    // all columns
+    for column in 0..6u8 {
+        for _ in 0..1000 {
+            for line in 0..6u8 {
+                board_leds.light_led_xy(column,line);
+                board_timer.delay_us(500_u16); // prevent "leaks" to same line LED
+            }
+        }
+    }
+    // all lines
+    for line in 0..6u8 {
+        for _ in 0..1000 {
+            for column in 0..6u8 {
+                board_leds.light_led_xy(column,line);
+                board_timer.delay_us(500_u16); // prevent "leaks" to same line LED
+            }
+        }
+    }
+}
+
 #[no_mangle]
 fn main() -> ! {
-    let (mut board_leds, mut board_timer) = new_board();
-    // first test animation
-    for _ in 0..2 {
-        for column in 0..6u8 {
-            for _ in 0..1000 {
-                for line in 0..6u8 {
-                    board_leds.light_led_xy(column,line);
-                    board_timer.delay_us(300_u16); // prevent "leaks" to same line LED
-                }
-            }
-        }
-    }
-    for _ in 0..2 {
-        for line in 0..6u8 {
-            for _ in 0..1000 {
-                for column in 0..6u8 {
-                    board_leds.light_led_xy(column,line);
-                    board_timer.delay_us(300_u16); // prevent "leaks" to same line LED
-                }
-            }
-        }
-    }
-    // show time!
+    let (board_leds, mut board_timer) = new_board();
+    demo(&board_leds, &board_timer);
     loop {
         board_timer.update_time();
         let (hour, min5, minute, seconds, tick) = board_timer.time();
@@ -39,7 +40,7 @@ fn main() -> ! {
             for mux_tick in 0..MAX_LEDS as u8 {
                 let led = led_multiplex(mux_tick, smooth_tick, hour, min5, minute, seconds, tick);
                 board_leds.light_led(led);
-                board_timer.delay_us(300_u16); // prevent "leaks" to same line LED
+                board_timer.delay_us(500_u16); // prevent "leaks" to same line LED
             }
         }
     }
